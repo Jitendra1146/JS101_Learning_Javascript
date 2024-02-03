bce8206b873c351cdd701f1f7ba1c9b4fb0a2e3c
Jitendra1146 <jitendraksj06@gmail.com>
2024-02-03 22:42:37 +0530

    mathematical oprations

---
+++ mathamatical_opration.js
@@ -0,0 +1,24 @@
+// Add
+
+let a = 5;
+let b = 10;
+let c = a+b;
+
+console.log(c)
+
+// Sub
+
+let num1 = 20;
+let num2 = 10;
+
+let num3 = num1 - num2;
+
+console.log(num3);
+
+// Multiply
+
+let n1 = 5;
+let n2 = 5;
+let n3 = n1 * n2;
+
+console.log(n3);
